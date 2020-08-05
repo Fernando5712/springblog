cb8d1ecc7e7bba0c41630abe893f1cf062be893f
Fernando Hernandez <Fernando.Hernandez.Fabars@gmail.com>
2020-08-05 11:39:33 -0500

    finished the jpa exercise

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,36 +1,33 @@
 package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.Post;
+import com.codeup.springblog.repositories.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
+
+    private final PostRepository postsDao;
+
+    public PostController(PostRepository postsDao) {
+        this.postsDao = postsDao;
+    }
+
     @GetMapping("/posts")
     public String index(Model model){
-//        creating a list of posts, to be hardcoded in with post records
-        ArrayList<Post> myPosts = new ArrayList<>();
-        myPosts.add(new Post(2,"Rick&Morty", "hi there"));
-        myPosts.add(new Post(3,"Obi Wan", "Hello, there"));
-        myPosts.add(new Post(4,"Scream", "WAAAASSSSSAAAAAAAAAA"));
-
-//        sending over the arraylist to iterate through in html
-        model.addAttribute("posts", myPosts);
+        model.addAttribute("posts", postsDao.findAll());
         return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
     public String show(@PathVariable long id, Model model){
-//        the PathVariable id gets passed into the constructor
-        Post myPost = new Post(id,"Test Title","Hello World!");
-        model.addAttribute("title", myPost.getTitle());
-        model.addAttribute("body", myPost.getBody());
+        Post pulledPost = postsDao.getOne(id);
+        model.addAttribute("post", pulledPost);
         return "posts/show";
     }
 
@@ -46,5 +43,33 @@ public class PostController {
         return "Post has been created!";
     }
 
+    @GetMapping("/posts/{id}/edit")
+    public String editForm(@PathVariable long id, Model model) {
+        model.addAttribute("post", postsDao.getOne(id));
+        return "posts/edit";
+    }
+
+    @PostMapping("/posts/{id}/edit")
+    public String update(@PathVariable long id,
+                         @RequestParam String title,
+                         @RequestParam String body) {
+
+        Post postToEdit = postsDao.getOne(id);
+
+        postToEdit.setTitle(title);
+
+        postToEdit.setBody(body);
+
+        postsDao.save(postToEdit);
+
+        // redirect show page for the given post
+        return "redirect:/posts/" + id;
+    }
+
+    @PostMapping("/posts/{id}/delete")
+    public String deletePost(@PathVariable long id) {
+        postsDao.deleteById(id);
+        return "redirect:/posts";
+    }
 
 }
---
+++ src/main/java/com/codeup/springblog/models/Dog.java
@@ -0,0 +1,23 @@
+//package com.codeup.springblog.models;
+//
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Table(name = "dogs")
+//public class Dog {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    public long id;
+//
+//    @Column(nullable = false, columnDefinition = "tinyint(3), unsigned")
+//    public int age;
+//
+//    @Column(nullable = false, length = 2)
+//    public String name;
+//
+//    @Column(nullable = false, columnDefinition = "char(2), DEFAULT, 'XX'")
+//    public String resideState;
+//}
+//
